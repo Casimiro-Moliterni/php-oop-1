00bf4ddf46bcb5d0a87792631b1f687d51c205d5
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-14 15:52:51 +0200

    add function construct

--- index.php
+++ index.php
@@ -6,17 +6,21 @@ public $actor;
 public $genre;
 public $date;
 
-
-
+public function __construct($_name,$_director,$_actor,$_genre,$_date){
+    $this->name = $_name;
+    $this->director = $_director;
+    $this->actor = $_actor;
+    $this->genre = $_genre;
+    $this->date = $_date;
+}
 }
 
-$il_cavaliere_oscuro = new Movie();
-$il_cavaliere_oscuro ->name = "Il Cavaliere Oscuro";
-$il_cavaliere_oscuro ->director =" Christopher Nolan";
-$il_cavaliere_oscuro ->actor =" Christian Bale";
-$il_cavaliere_oscuro ->genre = "action";
-$il_cavaliere_oscuro ->date ="23 luglio 2008";
+$il_cavaliere_oscuro = new Movie('Il Cavaliere Oscuro','Christopher Nolan','Christian Bale','action','23 luglio 2008');
+// $il_cavaliere_oscuro ->director =" Christopher Nolan";
+// $il_cavaliere_oscuro ->actor =" Christian Bale";
+// $il_cavaliere_oscuro ->genre = "action";
+// $il_cavaliere_oscuro ->date ="23 luglio 2008";
 
-var_dump(new Movie());
+
 var_dump($il_cavaliere_oscuro);
 ?>
