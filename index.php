66f1f0d7d8e8fb225fb40790da10972b1f390ae0
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-14 16:09:12 +0200

    add function get instance object in dom

--- index.php
+++ index.php
@@ -13,14 +13,22 @@ public function __construct($_name,$_director,$_actor,$_genre,$_date){
     $this->genre = $_genre;
     $this->date = $_date;
 }
+// funzione che genere intero movie 
+
+public function getFullMovie(){
+  $name = $this->name;
+  $director = $this->director;
+  $actor = $this->actor;
+  $genre=$this->genre;
+  $date=$this->date;
+  $fullMovie = $name . ' - ' . $director . ' - ' . $actor . ' - ' . $genre . ' - ' . $date;
+  return $fullMovie;
+}
+
 }
 
 $il_cavaliere_oscuro = new Movie('Il Cavaliere Oscuro','Christopher Nolan','Christian Bale','action','23 luglio 2008');
-// $il_cavaliere_oscuro ->director =" Christopher Nolan";
-// $il_cavaliere_oscuro ->actor =" Christian Bale";
-// $il_cavaliere_oscuro ->genre = "action";
-// $il_cavaliere_oscuro ->date ="23 luglio 2008";
-
+echo $il_cavaliere_oscuro->getFullMovie();
 
 var_dump($il_cavaliere_oscuro);
 ?>
