2284aefd626517aa6056ba5f71ec11a57a0eec1a
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-14 14:58:47 +0200

    add class Movie and istance

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-'test'
+<?php 
+class Movie{
+public $name;
+public $director;
+public $actor;
+public $genre;
+public $date;
+
+
+
+}
+
+$il_cavaliere_oscuro = new Movie();
+$il_cavaliere_oscuro ->name = "Il Cavaliere Oscuro";
+$il_cavaliere_oscuro ->director =" Christopher Nolan";
+$il_cavaliere_oscuro ->actor =" Christian Bale";
+$il_cavaliere_oscuro ->genre = "action";
+$il_cavaliere_oscuro ->date ="23 luglio 2008";
+
+var_dump(new Movie());
+var_dump($il_cavaliere_oscuro);
+?>
